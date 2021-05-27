dc2fba6d81e110428888be0c2390cf2c3c9bd0fa
RudRecciah <me@rudrecciah.dev>
2021-05-27 12:51:56 -0400

    testing

--- src/app.js
+++ src/app.js
@@ -65,8 +65,7 @@ ssl:
         this.sh.exec("apt install curl");
         console.log("Dependencies installed");
         console.log("Installing acme.sh...");
-        this.sh.exec("git clone https://github.com/acmesh-official/acme.sh.git");
-        this.sh.exec(`bash ./acme.sh/acme.sh --install --accountemail ${this.config.ssl.generatecerts.email} --home ../src/ssl/acme/ --cert-home ../src/ssl/certs`);
+        this.sh.exec(`wget -O -  https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh | bash ./acme.sh/acme.sh --install --accountemail ${this.config.ssl.generatecerts.email} --home ../src/ssl/acme/ --cert-home ../src/ssl/certs`);
         console.log("Acme.sh installed");
       }
     }catch(e) {
