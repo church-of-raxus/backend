456c9f21101a944afbd25abff0e3eddcbb061295
RudRecciah <me@rudrecciah.dev>
2021-05-30 17:30:07 -0400

    auth flow test 3

--- src/login/login.js
+++ src/login/login.js
@@ -1,5 +1,6 @@
-module.exports = function(config, uuid, exp, code) {
+module.exports = function(config, uuid, res, code) {
   const fetch = require("node-fetch");
+  //fetch discord token from auth code
   fetch("https://discord.com/api/oauth2/token", {
     method: "POST",
     body: new URLSearchParams({
@@ -13,17 +14,21 @@ module.exports = function(config, uuid, exp, code) {
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
     },
-  }).then(res => res.json()).then(json => {
-    console.log(json);
-    exp.send("yee haw");
-    // fetch("https://discord.com/api/users/@me", {
-    //   headers: {
-    //     authorization: `${type} ${code}`,
-    //   },
-    // }).then(res => res.json()).then(json => {
-    //   console.log(json);
-    //   res.send(JSON.stringify(json));
-    //   console.log(`Response to request ${uuid} sent from endpoint "/login/".`);
-    // });
+  }).then(data => data.json()).then(json => {
+    if(config.logging) {
+      console.log(json);
+    }
+    //fetch user metadata, and return the data needed on the frontend
+    fetch("https://discord.com/api/users/@me", {
+      headers: {
+        authorization: `${json.token_type} ${json.access_token}`,
+      },
+    }).then(data => data.json()).then(json => {
+      if(config.logging) {
+        console.log(json);
+      }
+      res.send(JSON.stringify(json));
+      console.log(`Response to request ${uuid} sent from endpoint "/login/".`);
+    });
   });
 };
