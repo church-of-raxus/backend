722e7280779d58f1a055ff9629bd253662367d9e
RudRecciah <me@rudrecciah.dev>
2021-05-31 02:29:35 -0400

    fix join date

--- src/userdata/generator.js
+++ src/userdata/generator.js
@@ -3,7 +3,8 @@ module.exports = function(id, data) {
   fs.writeJsonSync(`./data/users/${id}.json`, data);
   const file = fs.readJsonSync(`./data/users/${id}.json`);
   if(!("join" in file)) {
-    data.join = parseInt(Date().split(" ")[3]);
+    const date = Date().split(" ");
+    data.join = `${date[1]} ${date[2]} ${date[3]}`;
     fs.writeJsonSync(`./data/users/${id}.json`, data);
   }
   return data;
