473a6ca0c36ef035c343c7e8640d30c10d238b36
RudRecciah <me@rudrecciah.dev>
2021-06-01 00:34:37 -0400

    fix

--- src/userdata/coingetter.js
+++ src/userdata/coingetter.js
@@ -1,7 +1,7 @@
 module.exports = function(id) {
   const fs = require("fs-extra");
   if(fs.pathExistsSync(`./data/users/${id}.json`)) {
-    const file = fs.readJsonSync(`./data/users${id}.json`);
+    const file = fs.readJsonSync(`./data/users/${id}.json`);
     return file.bal;
   }else{
     return null;
