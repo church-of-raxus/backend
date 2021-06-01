adf5ed7c5277633c3a40d26ff01c7255430328d0
RudRecciah <me@rudrecciah.dev>
2021-06-01 10:46:44 -0400

    add type checking in setter

--- src/userdata/coinsetter.js
+++ src/userdata/coinsetter.js
@@ -1,17 +1,24 @@
 module.exports = function(id, positive, change) {
-  const fs = require("fs-extra");
-  const file = fs.readJsonSync(`./data/users/${id}.json`);
-  const oldBal = file.bal;
-  if(positive) {
-    file.bal = file.bal + change;
+  //returns old, new, and id if change is integer and successful, returns failure if not
+  if(typeof change === "bigint") {
+    const fs = require("fs-extra");
+    const file = fs.readJsonSync(`./data/users/${id}.json`);
+    const oldBal = file.bal;
+    if(positive) {
+      file.bal = file.bal + change;
+    }else{
+      file.bal = file.bal - change;
+    }
+    fs.writeJsonSync(`./data/users/${id}.json`, file);
+    return {
+      "success": true,
+      "id": file.id,
+      "oldBal": oldBal,
+      "newBal": file.bal
+    };
   }else{
-    file.bal = file.bal - change;
+    return {
+      "success": false
+    };
   }
-  fs.writeJsonSync(`./data/users/${id}.json`, file);
-  return {
-    "success": true,
-    "id": file.id,
-    "oldBal": oldBal,
-    "newBal": file.bal
-  };
 };
