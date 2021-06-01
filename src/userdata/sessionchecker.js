87c4e1fcb1756c03e0c05a551ef65233eb0d74bb
RudRecciah <me@rudrecciah.dev>
2021-06-01 10:14:47 -0400

    coin setter fix 2

--- src/userdata/sessionchecker.js
+++ src/userdata/sessionchecker.js
@@ -5,6 +5,7 @@ module.exports = function(id, session) {
   //time and hash
   const time = Date.now();
   session = hasher(session);
+  console.log(session);
   session = JSON.parse(session);
   if(fs.pathExistsSync(`./data/users/${id}.json`)) {
     const file = fs.readJsonSync(`./data/users/${id}.json`);
