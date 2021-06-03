6ed1d6d2fba965d4d6c4c4dd3e6b12de91d876f1
RudRecciah <me@rudrecciah.dev>
2021-06-02 23:00:50 -0400

    remove console logs

--- src/userdata/generator.js
+++ src/userdata/generator.js
@@ -1,21 +1,17 @@
 module.exports = function(id, data, session) {
   const fs = require("fs-extra");
   const file = fs.readJsonSync(`./data/users/${id}.json`);
-  console.log(data);
-  console.log(file);
   if("join" in file) {
     data.join = file.join;
   }else{
     const date = Date().split(" ");
     data.join = `${date[1]} ${date[2]} ${date[3]}`;
   }
-  console.log(data);
   if("bal" in file) {
     data.bal = file.bal;
   }else{
     data.bal = 0;
   }
-  console.log(data);
   fs.writeJsonSync(`./data/users/${id}.json`, data);
   data.session = session;
   delete data.timeout;
