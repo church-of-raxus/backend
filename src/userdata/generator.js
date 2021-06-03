9f5d5d806b119e26c05325684afc3bd51d267f9b
RudRecciah <me@rudrecciah.dev>
2021-06-02 21:19:34 -0400

    test 1

--- src/userdata/generator.js
+++ src/userdata/generator.js
@@ -2,13 +2,16 @@ module.exports = function(id, data, session) {
   const fs = require("fs-extra");
   fs.writeJsonSync(`./data/users/${id}.json`, data);
   const file = fs.readJsonSync(`./data/users/${id}.json`);
+  console.log(data);
   if(!("join" in file)) {
     const date = Date().split(" ");
     data.join = `${date[1]} ${date[2]} ${date[3]}`;
   }
+  console.log(data);
   if(!("bal" in file)) {
     data.bal = 0;
   }
+  console.log(data);
   fs.writeJsonSync(`./data/users/${id}.json`, data);
   data.session = session;
   delete data.timeout;
