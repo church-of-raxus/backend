012f7cb13567aa76a53d75ff9d30258a8b3eb041
RudRecciah <me@rudrecciah.dev>
2021-06-02 21:40:10 -0400

    fix login userdata reset bug

--- src/userdata/generator.js
+++ src/userdata/generator.js
@@ -1,6 +1,5 @@
 module.exports = function(id, data, session) {
   const fs = require("fs-extra");
-  fs.writeJsonSync(`./data/users/${id}.json`, data);
   const file = fs.readJsonSync(`./data/users/${id}.json`);
   console.log(data);
   console.log(file);
