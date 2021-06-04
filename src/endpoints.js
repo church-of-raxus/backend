872b21790df688870afedd64e5d0ce5e455651a8
RudRecciah <me@rudrecciah.dev>
2021-06-04 09:22:22 -0400

    remove coin get entirely

--- src/endpoints.js
+++ src/endpoints.js
@@ -27,19 +27,6 @@ module.exports = class {
       const responseBody = require("./login/login.js");
       responseBody(config, uuid, res, data.code);
     });
-    
-    //coin getter endpoint
-    server.post("/coin/get/", (req, res) => {
-      const uuid = this.uuid.v4();
-      console.log(`New request to endpoint "/coin/get/". ID: ${uuid}`);
-      res.setHeader("Content-Type", "text/plain");
-      if(config.logging) {
-        console.log(req.body);
-      }
-      const data = JSON.parse(req.body);
-      const responseBody = require("./coin/get.js");
-      responseBody(config, uuid, data.id, res);
-    });
   
     //coin setter endpoint
     server.post("/coin/set/", (req, res) => {
