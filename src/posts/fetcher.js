d42ef130a9379811cbc0ee949f087530c79bd5d8
RudRecciah <me@rudrecciah.dev>
2021-07-08 16:24:51 -0400

    fetcher v2

--- src/posts/fetcher.js
+++ src/posts/fetcher.js
@@ -2,13 +2,13 @@ module.exports = function(uuid, current, res) {
   "use strict";
   const fs = require("fs-extra");
   let body = {posts:[]};
-  if(fs.exists(`./data/posts/${current + 1}.json`)) {
+  if(fs.pathExistsSync(`./data/posts/${current + 1}.json`)) {
     body.posts.first = fs.readJsonSync(`./data/posts/${current + 1}.json`);
   }
-  if(fs.exists(`./data/posts/${current + 2}.json`)) {
+  if(fs.pathExistsSync(`./data/posts/${current + 2}.json`)) {
     body.posts.second = fs.readJsonSync(`./data/posts/${current + 2}.json`);
   }
-  if(fs.exists(`./data/posts/${current + 3}.json`)) {
+  if(fs.pathExistsSync(`./data/posts/${current + 3}.json`)) {
     body.posts.third = fs.readJsonSync(`./data/posts/${current + 3}.json`);
   }
   res.send(body);
