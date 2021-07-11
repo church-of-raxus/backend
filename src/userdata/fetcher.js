1a8e8cb40f72e49eb66508372d4b5ddce4c16cf7
RudRecciah <me@rudrecciah.dev>
2021-07-10 21:01:05 -0400

    swap data folder to parent of run folder

--- src/userdata/fetcher.js
+++ src/userdata/fetcher.js
@@ -2,8 +2,8 @@ module.exports = function(uuid, user, res) {
   "use strict";
   //check if user exists, if so return user's name and pfp url, if not return false success
   const fs = require("fs-extra");
-  if(fs.pathExistsSync(`./data/users/${user}.json`)) {
-    const data = fs.readJsonSync(`./data/users/${user}.json`);
+  if(fs.pathExistsSync(`../data/users/${user}.json`)) {
+    const data = fs.readJsonSync(`../data/users/${user}.json`);
     res.send(JSON.stringify({
       success: true,
       id: data.id,
